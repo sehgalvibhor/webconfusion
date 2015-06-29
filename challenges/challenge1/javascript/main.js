6d465fc54cd3af13c6b1125b167fa167ad01b168
Utkarsh Dhawan <dhawan.utkarsh@gmail.com>
2015-06-29 21:59:53 +0530

    Deleted Uneccessary files

--- challenges/challenge1/javascript/main.js
+++
@@ -1,8 +0,0 @@
-function sidemenu(){
-	document.getElementById('button-side-menu').style.display="none";
-	document.getElementById('side-menu').style.display="block";
-}
-function closesidemenu(){
-	document.getElementById('button-side-menu').style.display="block";
-	document.getElementById('side-menu').style.display="none";
-}
